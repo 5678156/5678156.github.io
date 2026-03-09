6922e12a24c34579d93fdf04848a750f0bed3072
Davi Sancho <davirsancho@gmail.com>
2026-03-09 08:54:46 -0400

    game.js

--- projects/game/game.js
+++ projects/game/game.js
@@ -1,4 +1,19 @@
-let gameActive = true; //this variable is required. 
+let gameActive = true; 
+let haveKey = false;
+let openDoor = false;
+let day = 0;
+let gameActive = true;
+let lobbyDiscovered = true;
+let LoungeDiscovered = false;
+let BathroomDiscovered = false;
+let GymDiscovered = false;
+let GameRoomDiscovered = false;
+let SecretRoomDiscovered = false;
+
+
+
+
+//this variable is required. 
                        //to stop the game, set it to false.
 
 //Declare your other global variables here
@@ -8,37 +23,89 @@ let gameActive = true; //this variable is required.
 
 
 //Make one function for each location
-function locationA() {
+function lobby() 
+{
     clear();
-    print("\nYou are in location A!");
+    print("\nYou are in the lobby!");
+	print
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationB");
+        "\n\tLounge");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationb") {
-            locationB();
+        if (input.toLowerCase() === "lounge") {
+            Lounge();
         } else {
             stayHere();
-            waitThenCall(locationA);
+            waitThenCall(lobby);
+	    print("\nYou have been caught by Mason!");
+		print("\nYou lost!")
         }
     }
+  if (day < 5) {
+        print("\nwould you like to try again tomorrow? Say yes or no");
+        function processInput(input){
+            if (input.toLowerCase() === "yes") {
+                day++;
+                minutes = 0;
+                haveCup = false;
+                cupIsFull = false;
+                start();
+            } else if (input.toLowerCase() === "no") {
+                print("\nok, better luck next time!");
+                gameActive = false;
+            } else {
+                print("Please type 'yes' or 'no'");
+            }
+        }
+
+        waitForInput(processInput);
+    } else {
+        gameActive = false;
+    }
+}
+
     waitForInput(processInput);
 }
 
-function locationB() {
+function Lounge() {
     clear();
-    print("\nYou are in location B!");
+    print("\nYou are in the Lounge!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
+        "\n\tlobby");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationa") {
-            locationA();
+        if (input.toLowerCase() === "lobby") {
+            lobby();
         } else {
             stayHere();
-            waitThenCall(locationB);
+            waitThenCall(Lounge);
+		print("\nMason caught youuuuu!")
+		print("\nYou lose!")
         }
     }
+  if (day < 5) {
+        print("\nwould you like to try again tomorrow? Say yes or no");
+        function processInput(input){
+            if (input.toLowerCase() === "yes") {
+                day++;
+                minutes = 0;
+                haveCup = false;
+                cupIsFull = false;
+                start();
+            } else if (input.toLowerCase() === "no") {
+                print("\nok, better luck next time!");
+                gameActive = false;
+            } else {
+                print("Please type 'yes' or 'no'");
+            }
+        }
+
+        waitForInput(processInput);
+    } else {
+        gameActive = false;
+    }
+}
+
     waitForInput(processInput);
 }
 
@@ -46,10 +113,15 @@ function locationB() {
 //very start. For this simple example, any input will bring you
 //to locationA
 function start(){
-    print("Welcome to my game! Press any key to start");
+    print("Welcome to Mason's Demise! Type start to start");
 
     function processInput(input){
-            locationA();
+	    if (input.toLowerCase() === "start") {
+            lobby();
+	   } else {
+		   print("\nError! " +
+			   "You have to type start to start!");
+		   print("\nPleaseee type start this time!")
+		   
     }
-    waitForInput(processInput);
-}
+    waitForInput(processInput)
