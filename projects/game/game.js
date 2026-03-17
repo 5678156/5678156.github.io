a57d80686571d09693728406345e87aba5fb8be9
Davi Sancho <davirsancho@gmail.com>
2026-03-16 20:14:57 -0400

    error

--- projects/game/game.js
+++ projects/game/game.js
@@ -1,18 +1,4 @@
-let haveKey = false;
-let openDoor = false;
-let day = 0;
-let gameActive = true;
-let lobbyDiscovered = true;
-let LoungeDiscovered = false;
-let BathroomDiscovered = false;
-let GymDiscovered = false;
-let GameRoomDiscovered = false;
-let SecretRoomDiscovered = false;
-
-
-
-
-//this variable is required. 
+let gameActive = true; //this variable is required. 
                        //to stop the game, set it to false.
 
 //Declare your other global variables here
@@ -22,104 +8,48 @@ let SecretRoomDiscovered = false;
 
 
 //Make one function for each location
-function lobby() 
-{
+function locationA() {
     clear();
-    print("\nYou are in the lobby!");
-	print
+    print("\nYou are in location A!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tLounge");
+        "\n\tlocationB");
     
     function processInput(input){
-        if (input.toLowerCase() === "lounge") {
-            Lounge();
+        if (input.toLowerCase() === "locationb") {
+            locationB();
         } else {
             stayHere();
-            waitThenCall(lobby);
-	    print("\nYou have been caught by Mason!");
-		print("\nYou lost!")
+            waitThenCall(locationA);
         }
     }
-  if (day < 5) {
-        print("\nwould you like to try again tomorrow? Say yes or no");
-        function processInput(input){
-            if (input.toLowerCase() === "yes") {
-                day++;
-                minutes = 0;
-                haveCup = false;
-                cupIsFull = false;
-                start();
-            } else if (input.toLowerCase() === "no") {
-                print("\nok, better luck next time!");
-                gameActive = false;
-            } else {
-                print("Please type 'yes' or 'no'");
-            }
-        }
-
-        waitForInput(processInput);
-    } else {
-        gameActive = false;
-    }
-
-
     waitForInput(processInput);
 }
 
-function Lounge() {
+function locationB() {
     clear();
-    print("\nYou are in the Lounge!");
+    print("\nYou are in location B!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlobby");
+        "\n\tlocationA");
     
     function processInput(input){
-        if (input.toLowerCase() === "lobby") {
-            lobby();
+        if (input.toLowerCase() === "locationa") {
+            locationA();
         } else {
             stayHere();
-            waitThenCall(Lounge);
-		print("\nMason caught youuuuu!")
-		print("\nYou lose!")
+            waitThenCall(locationB);
         }
     }
-  if (day < 5) {
-        print("\nwould you like to try again tomorrow? Say yes or no");
-        function processInput(input){
-            if (input.toLowerCase() === "yes") {
-                day++;
-                minutes = 0;
-                haveCup = false;
-                cupIsFull = false;
-                start();
-            } else if (input.toLowerCase() === "no") {
-                print("\nok, better luck next time!");
-                gameActive = false;
-            } else {
-                print("Please type 'yes' or 'no'");
-            }
-        }
-
-    } else {
-        gameActive = false;
-    }
-}
     waitForInput(processInput);
-}
 }
 
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
 function start(){
-    print("Welcome to Mason's Demise! Type start to start");
+    print("Welcome to my game! Press any key to start");
 
     function processInput(input){
-	    if (input.toLowerCase() === "start") {
-            lobby();
-	   } else {
-		   print("\nError! " +
-			   "You have to type start to start!");
-		   print("\nPleaseee type start this time!")
-		   
+            locationA();
     }
-    waitForInput(processInput)
+    waitForInput(processInput);
+}
